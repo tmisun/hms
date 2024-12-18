c4b3374115c679eae084fbc3d377f68f60d1dc02
tmisun <t.misun@gmail.com>
2024-12-18 13:59:58 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
         loader: 'imgix',
         path: 'https://tmisun.github.io/sun/',
     },
+    assetPrefix: 'https://tmisun.github.io/sun/',
 };
 
 export default nextConfig;
